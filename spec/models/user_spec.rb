8a9e2d168225ef978f74a368377c24df83ccae55
Antonio King Hunt <tesseractcode@icloud.com>
2023-11-15 19:13:04 -0700

    Initial commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :phone_number }
+    it { should validate_presence_of :password_digest }
+    it { should validate_presence_of :email }
+    it { should have_secure_password }
+
+    it 'is invalid with a password shorter than 8 characters' do
+      invalid_pw = Faker::Internet.password(min_length: 6, max_length: 7)
+      user = FactoryBot.build(:user, password: invalid_pw, password_confirmation: invalid_pw)
+
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include('is too short (minimum is 8 characters)')
+    end
+  end
 end
