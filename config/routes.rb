1bc49f42341c961582ae81c83bd3f9ef3edf823a
Antonio King Hunt <tesseractcode@icloud.com>
2023-12-11 18:02:24 -0700

    React on rails install successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
